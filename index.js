03d5bb436995a14d5a5d52f11302c0b53b19188b
Zack Jordan <=>
2019-10-21 16:40:24 -0500

    Add AsyncStatus.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import * as Actions from './src/js/actions/actions';
 import * as TrellisActions from './src/js/actions/trellisActions';
 import * as StargazerActions from './src/js/actions/stargazerActions';
+import AsyncStatus from './src/js/constants/AsyncStatus';
 
 module.exports = {
   ...Actions,
   ...TrellisActions,
-  ...StargazerActions
+  ...StargazerActions,
+  AsyncStatus
 };
