43dfaa37e6e00c25461d7cc7486d6d08e9f539ab
Zack Jordan <=>
2019-10-21 16:54:10 -0500

    Last try to get the exporting right.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import * as Actions from './src/js/actions/actions';
 import * as TrellisActions from './src/js/actions/trellisActions';
 import * as StargazerActions from './src/js/actions/stargazerActions';
-import AsyncStatus from './src/js/constants/AsyncStatus';
+import { AsyncStatus } from './src/js/constants/AsyncStatus';
 
 module.exports = {
   ...Actions,
