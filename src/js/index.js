a372448c0e29b6ff20b892c7aa2bf6ed6df6e2fe
Zack Jordan <=>
2019-10-22 14:48:30 -0500

    Change export strategy once again.

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ const Actions = require('./actions/actions');
 const TrellisActions = require('./actions/trellisActions');
 const StargazerActions = require('./actions/stargazerActions');
 const SnapActions = require('./actions/snapActions');
-const AsyncStatus = require('./constants/AsyncStatus');
+const { default: AsyncStatus } = require('./constants/AsyncStatus');
 
 export {
   Actions,
