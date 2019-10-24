b40b92f4e79e878cb3581790ad490be45b1755b4
Zack Jordan <=>
2019-10-24 12:53:07 -0500

    Add loaders to export.

--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,15 @@ const StargazerActions = require('./actions/stargazerActions');
 const SnapActions = require('./actions/snapActions');
 const { default: AsyncStatus } = require('./constants/AsyncStatus');
 const { default: AsyncMethods } = require('./constants/AsyncMethods');
+const fetchLoader = require('./loaders/fetchLoader');
+const ioLoader = require('./loaders/ioLoader');
 
 export {
   Actions,
-  createTrellisActions,
   StargazerActions,
   SnapActions,
   AsyncStatus,
   AsyncMethods,
+  fetchLoader,
+  ioLoader
 };
